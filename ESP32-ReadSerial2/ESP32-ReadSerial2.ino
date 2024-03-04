81368f80305d3a54dca714de4c35d303a1405543
Tail_BackShape <130843271+Tail-BackShape@users.noreply.github.com>
2024-03-04 15:41:49 +0900

    add ESP32-ReadSerial2\ESP32-ReadSerial2.ino
    Interrupt output of Serial 2 signal

---
+++ ESP32-ReadSerial2/ESP32-ReadSerial2.ino
@@ -0,0 +1,42 @@
+int LED = 18;
+int count = 0;
+
+void serialEvent2();
+void twinkle();
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  Serial.println(" ");
+  Serial2.begin(115200);
+
+  delay(1000);
+}
+
+void loop() {
+  twinkle();
+  count++;
+  delay(100);
+}
+
+void twinkle() {
+  if ((count % 2) == 1) {
+    digitalWrite(LED, HIGH);
+  } else {
+    digitalWrite(LED, LOW);
+  }
+  Serial.print("count = ");
+  Serial.print(count);
+  Serial.print("\n");
+}
+
+void serialEvent2() {
+  if (Serial2.available() > 0) {
+    // シリアルデータの受信 (改行まで)
+    String data = Serial2.readStringUntil('\n');
+
+    // 受信したデータを出力
+    Serial.print("Receive:");
+    Serial.println(data);
+  }
+}
