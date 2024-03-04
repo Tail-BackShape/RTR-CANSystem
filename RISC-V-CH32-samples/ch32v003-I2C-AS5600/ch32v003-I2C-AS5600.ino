309bf0910dca64aec2480cf98b9788ae3e4c11b8
Tail_BackShape <130843271+Tail-BackShape@users.noreply.github.com>
2024-03-04 15:43:39 +0900

    add RISC-V-CH32-samples\ch32v003-I2C-AS5600\ch32v003-I2C-AS5600.ino
    AS5600 value is obtained via I2C and passed through to the UART signal.

--- RISC-V-CH32-samples/ch32v003-I2C-AS5600/ch32v003-I2C-AS5600.ino
+++ RISC-V-CH32-samples/ch32v003-I2C-AS5600/ch32v003-I2C-AS5600.ino
@@ -1,35 +1,23 @@
 #include <Wire.h>
 #define AS5600_addr 0x36
 
+
 // Function Prototype Declaration
+byte read_AS5600(byte);
+void write_AS5600(byte, byte);
 
-byte read_AS5600(byte addr) {
-  Wire.beginTransmission(AS5600_addr);
-  Wire.write(addr);
-  Wire.endTransmission(false);
-  Wire.requestFrom(AS5600_addr, 1, true);
-  byte data = Wire.read();
-  return data;
-}
-
-void write_AS5600(byte addr, byte data) {
-  Wire.beginTransmission(AS5600_addr);
-  Wire.write(addr);  // addressing
-  Wire.write(data);  // write data
-  Wire.endTransmission();
-}
-
-void setup() {
-  //Wire.setSDA(PC1);
-  //Wire.setSCL(PC2);
-  Wire.setClock(10000);
-  Wire.begin();
-
-  Serial.begin(9600);
-
+void setup()
+{
   pinMode(PD4, OUTPUT);
 
+  Wire.begin();
+  Serial.begin(115200);
+  while (!Serial)
+  {
+    delay(1);
+  }
 
+  delay(1000);
   byte offSetAngle1 = read_AS5600(0x0C);
   byte offSetAngle2 = read_AS5600(0x0D);
 
@@ -37,10 +25,8 @@ void setup() {
   write_AS5600(0x02, offSetAngle2);
 }
 
-void loop() {
-  digitalWrite(PD4, LOW);
-  delay(40);
-
+void loop()
+{
   // check status
   byte AS5600Status = read_AS5600(0x0B);
   AS5600Status = AS5600Status & 0B00111000;
@@ -49,43 +35,69 @@ void loop() {
   uint16_t rawAngle1 = read_AS5600(0x0C);
   uint16_t rawAngle2 = read_AS5600(0x0D);
   uint16_t rawAngle = ((0x0F & rawAngle1) << 8) | rawAngle2;
-  delay(40);
 
   // read angle
   uint16_t angle1 = read_AS5600(0x0E);
   uint16_t angle2 = read_AS5600(0x0F);
   uint16_t angle = ((0x0F & angle1) << 8) | angle2;
-  delay(40);
 
   // read ZPOS
   uint16_t zpos1 = read_AS5600(0x01);
   uint16_t zpos2 = read_AS5600(0x02);
   uint16_t zpos = ((0x0F & zpos1) << 8) | zpos2;
-  delay(40);
 
   // read MPOS
   uint16_t mpos1 = read_AS5600(0x03);
   uint16_t mpos2 = read_AS5600(0x04);
   uint16_t mpos = ((0x0F & mpos1) << 8) | mpos2;
-  delay(40);
 
-  Serial.write("Status:");
-  Serial.write(AS5600Status);
-  Serial.write("\n");
-  Serial.write("RAWAngel:");
-  Serial.write(rawAngle);
-  Serial.write("\n");
-  Serial.write("Angle:");
-  Serial.write(angle);
-  Serial.write("\n");
-  Serial.write("ZPOS:");
-  Serial.write(zpos);
-  Serial.write("\n");
-  Serial.write("MPOS:");
-  Serial.write(mpos);
-  Serial.write("\n");
-  Serial.write("-----------------");
-  Serial.write("\n");
-  digitalWrite(PD4, HIGH);
+  Serial.print("Status:");
+  Serial.print(AS5600Status);
+  Serial.print("\n");
+
+  Serial.print("RAWAngel:");
+  Serial.print(rawAngle);
+  Serial.print("\n");
+
+  Serial.print("Angle:");
+  Serial.print(angle);
+  Serial.print("\n");
+
+  Serial.print("ZPOS:");
+  Serial.print(zpos);
+  Serial.print("\n");
+
+  Serial.print("MPOS:");
+  Serial.print(mpos);
+  Serial.print("-----------------");
+  Serial.print("\n");
+
+  Ltika();
+
   delay(20);
+}
+
+byte read_AS5600(byte addr)
+{
+  Wire.beginTransmission(AS5600_addr);
+  Wire.write(addr);
+  Wire.endTransmission(false);
+  Wire.requestFrom(AS5600_addr, 1, true);
+  byte data = Wire.read();
+  return data;
+}
+
+void write_AS5600(byte addr, byte data)
+{
+  Wire.beginTransmission(AS5600_addr);
+  Wire.write(addr); // addressing
+  Wire.write(data); // write data
+  Wire.endTransmission();
+}
+
+void Ltika()
+{
+  digitalWrite(PD4, HIGH);
+  delay(80);
+  digitalWrite(PD4, LOW);
 }
