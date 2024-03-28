4759deff55645a6660bb3f6e6bbccd7e96ae4309
Tail_BackShape <130843271+Tail-BackShape@users.noreply.github.com>
2024-03-28 14:41:00 +0900

    おにぎり

---
+++ I2UACAN-Test2/Test2-ch32v003-deg2uart/Test2-ch32v003-deg2uart.ino
@@ -0,0 +1,60 @@
+#include <Wire.h>
+#define AS5600_addr 0x36
+
+// Function Prototype Declaration
+byte read_AS5600(byte);
+void write_AS5600(byte, byte);
+
+void setup()
+{
+  Wire.begin();
+  Serial.begin(115200);
+  while (!Serial)
+  {
+    delay(1);
+  }
+
+  delay(1000);
+  byte offSetAngle1 = read_AS5600(0x0C);
+  byte offSetAngle2 = read_AS5600(0x0D);
+
+  write_AS5600(0x01, offSetAngle1);
+  write_AS5600(0x02, offSetAngle2);
+}
+
+void loop()
+{
+  // check status
+  byte AS5600Status = read_AS5600(0x0B);
+  AS5600Status = AS5600Status & 0B00111000;
+
+  // read angle
+  uint16_t angle1 = read_AS5600(0x0E);
+  uint16_t angle2 = read_AS5600(0x0F);
+  uint16_t angle = ((0x0F & angle1) << 8) | angle2;
+  float angledeg = angle * (360.0 / 4096.0);
+
+  Serial.print("AngleDeg:");
+  Serial.println(angledeg);
+  Serial.println("-----------------");
+
+  delay(20);
+}
+
+byte read_AS5600(byte addr)
+{
+  Wire.beginTransmission(AS5600_addr);
+  Wire.write(addr);
+  Wire.endTransmission(false);
+  Wire.requestFrom(AS5600_addr, 1, true);
+  byte data = Wire.read();
+  return data;
+}
+
+void write_AS5600(byte addr, byte data)
+{
+  Wire.beginTransmission(AS5600_addr);
+  Wire.write(addr); // addressing
+  Wire.write(data); // write data
+  Wire.endTransmission();
+}
